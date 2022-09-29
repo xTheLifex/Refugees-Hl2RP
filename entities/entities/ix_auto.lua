7bef9a90297a54d1df75a53cfd6b9fb44fd5c5bb
xTheLifex <nicholastoya@yahoo.com.br>
2022-09-29 16:58:15 -0300

    ix_auto

---
+++ entities/entities/ix_auto.lua
@@ -0,0 +1,21 @@
+AddCSLuaFile()
+ENT.Base = "base_entity"
+ENT.Type = "point"
+
+if (SERVER) then
+    local ix = ix or nil
+
+    hook.Add("InitializedPlugins", "ixAutoExecute", function() 
+        timer.Simple(5, function()  -- Wait a bit for everything to be properly loaded.
+            for _, v in ipairs(ents.FindByClass("ix_auto")) do
+                v:TriggerOutput("OnPluginsLoaded", v)
+            end
+        end)
+    end)
+    
+    function ENT:KeyValue(key, value)
+        if(key == "OnPluginsLoaded") then
+            self:StoreOutput(key, value)
+        end
+    end
+end
