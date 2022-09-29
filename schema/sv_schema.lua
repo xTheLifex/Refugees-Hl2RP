29587a5bc8bb334b541125c230f1be61291b21ff
xTheLifex <nicholastoya@yahoo.com.br>
2022-09-29 17:00:45 -0300

    Schema ents FGD support

--- schema/sv_schema.lua
+++ schema/sv_schema.lua
@@ -27,7 +27,7 @@ function Schema:SaveRationDispensers()
 	local data = {}
 
 	for _, v in ipairs(ents.FindByClass("ix_rationdispenser")) do
-		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetEnabled()}
+		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetEnabled(), v:MapCreationID()}
 	end
 
 	ix.data.Set("rationDispensers", data)
@@ -37,7 +37,7 @@ function Schema:SaveVendingMachines()
 	local data = {}
 
 	for _, v in ipairs(ents.FindByClass("ix_vendingmachine")) do
-		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetAllStock()}
+		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetAllStock(), v:MapCreationID()}
 	end
 
 	ix.data.Set("vendingMachines", data)
@@ -52,7 +52,8 @@ function Schema:SaveCombineLocks()
 				v.door:MapCreationID(),
 				v.door:WorldToLocal(v:GetPos()),
 				v.door:WorldToLocalAngles(v:GetAngles()),
-				v:GetLocked()
+				v:GetLocked(),
+				v:MapCreationID(),
 			}
 		end
 	end
@@ -64,7 +65,7 @@ function Schema:SaveForceFields()
 	local data = {}
 
 	for _, v in ipairs(ents.FindByClass("ix_forcefield")) do
-		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetMode()}
+		data[#data + 1] = {v:GetPos(), v:GetAngles(), v:GetMode(), v:MapCreationID()}
 	end
 
 	ix.data.Set("forceFields", data)
@@ -73,49 +74,75 @@ end
 -- data loading
 function Schema:LoadRationDispensers()
 	for _, v in ipairs(ix.data.Get("rationDispensers") or {}) do
-		local dispenser = ents.Create("ix_rationdispenser")
-
-		dispenser:SetPos(v[1])
-		dispenser:SetAngles(v[2])
-		dispenser:Spawn()
-		dispenser:SetEnabled(v[3])
+		local mapid = v[4] or -1
+		if (mapid > -1) then
+			-- This was created by the map, we dont need to spawn a new one.
+			local dispenser = ents.GetMapCreatedEntity(mapid)
+			dispenser:SetEnabled(v[3])
+		else
+			local dispenser = ents.Create("ix_rationdispenser")
+			dispenser:SetPos(v[1])
+			dispenser:SetAngles(v[2])
+			dispenser:Spawn()
+			dispenser:SetEnabled(v[3])
+		end
 	end
 end
 
 function Schema:LoadVendingMachines()
 	for _, v in ipairs(ix.data.Get("vendingMachines") or {}) do
-		local vendor = ents.Create("ix_vendingmachine")
+		local mapid = v[4] or -1
+		if (mapid > -1) then
+			-- This was created by the map, we dont need to spawn a new one.
+			local vendor = ents.GetMapCreatedEntity(mapid)
+			vendor:SetStock(v[3])
+		else
+			local vendor = ents.Create("ix_vendingmachine")
 
-		vendor:SetPos(v[1])
-		vendor:SetAngles(v[2])
-		vendor:Spawn()
-		vendor:SetStock(v[3])
+			vendor:SetPos(v[1])
+			vendor:SetAngles(v[2])
+			vendor:Spawn()
+			vendor:SetStock(v[3])
+		end
 	end
 end
 
 function Schema:LoadCombineLocks()
 	for _, v in ipairs(ix.data.Get("combineLocks") or {}) do
-		local door = ents.GetMapCreatedEntity(v[1])
-
-		if (IsValid(door) and door:IsDoor()) then
-			local lock = ents.Create("ix_combinelock")
-
-			lock:SetPos(door:GetPos())
-			lock:Spawn()
-			lock:SetDoor(door, door:LocalToWorld(v[2]), door:LocalToWorldAngles(v[3]))
+		local mapid = v[5] or -1
+		if (mapid > -1) then
+			-- This was created by the map, we dont need to spawn a new one.
+			local lock = ents.GetMapCreatedEntity(mapid)
 			lock:SetLocked(v[4])
-		end
+		else
+			local door = ents.GetMapCreatedEntity(v[1])
+
+			if (IsValid(door) and door:IsDoor()) then
+				local lock = ents.Create("ix_combinelock")
+	
+				lock:SetPos(door:GetPos())
+				lock:Spawn()
+				lock:SetDoor(door, door:LocalToWorld(v[2]), door:LocalToWorldAngles(v[3]))
+				lock:SetLocked(v[4])
+			end
+		end 
 	end
 end
 
 function Schema:LoadForceFields()
 	for _, v in ipairs(ix.data.Get("forceFields") or {}) do
-		local field = ents.Create("ix_forcefield")
+		local mapid = v[4] or -1
+		if (mapid > -1) then
+			local field = ents.GetMapCreatedEntity(mapid)
+			field:SetMode(v[3])
+		else
+			local field = ents.Create("ix_forcefield")
 
-		field:SetPos(v[1])
-		field:SetAngles(v[2])
-		field:Spawn()
-		field:SetMode(v[3])
+			field:SetPos(v[1])
+			field:SetAngles(v[2])
+			field:Spawn()
+			field:SetMode(v[3])
+		end
 	end
 end
 
