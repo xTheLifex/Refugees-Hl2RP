b39b9725a485248a6d64dcdc15eba69c92b40faa
xTheLifex <nicholastoya@yahoo.com.br>
2022-10-01 21:10:08 -0300

    ix_auto fix

--- entities/entities/ix_auto.lua
+++ entities/entities/ix_auto.lua
@@ -12,6 +12,14 @@ if (SERVER) then
             end
         end)
     end)
+
+    hook.Add("PostCleanupMap", "ixAutoCleanup", function() 
+        timer.Simple(5, function()  -- Wait a bit for everything to be properly loaded.
+            for _, v in ipairs(ents.FindByClass("ix_auto")) do
+                v:TriggerOutput("OnPluginsLoaded", v)
+            end
+        end)
+    end)
     
     function ENT:KeyValue(key, value)
         if(key == "OnPluginsLoaded") then
