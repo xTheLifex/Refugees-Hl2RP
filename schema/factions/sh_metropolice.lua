4a335db6f2b23f9230fe9b595ea0d5f244d4d749
Stryker4747 <115424513+Stryker4747@users.noreply.github.com>
2022-10-09 21:05:46 +0200

    Enabled class selection.

--- schema/factions/sh_metropolice.lua
+++ schema/factions/sh_metropolice.lua
@@ -27,29 +27,20 @@ function FACTION:OnTransferred(character)
 end
 
 function FACTION:OnNameChanged(client, oldValue, value)
-	--local character = client:GetCharacter()
-	--[[
+	local character = client:GetCharacter()
+	[[
 	if (!Schema:IsCombineRank(oldValue, "RCT") and Schema:IsCombineRank(value, "RCT")) then
 		character:JoinClass(CLASS_MPR)
-	elseif (!Schema:IsCombineRank(oldValue, "OfC") and Schema:IsCombineRank(value, "OfC")) then
-		character:SetModel("models/policetrench.mdl")
 	elseif (!Schema:IsCombineRank(oldValue, "EpU") and Schema:IsCombineRank(value, "EpU")) then
 		character:JoinClass(CLASS_EMP)
-
-		character:SetModel("models/leet_police2.mdl")
 	elseif (!Schema:IsCombineRank(oldValue, "DvL") and Schema:IsCombineRank(value, "DvL")) then
 		character:SetModel("models/eliteshockcp.mdl")
-	elseif (!Schema:IsCombineRank(oldValue, "SeC") and Schema:IsCombineRank(value, "SeC")) then
+	elseif (!Schema:IsCombineRank(oldValue, "CmD") and Schema:IsCombineRank(value, "CmD")) then
 		character:SetModel("models/sect_police2.mdl")
 	elseif (!Schema:IsCombineRank(oldValue, "SCN") and Schema:IsCombineRank(value, "SCN")
 	or !Schema:IsCombineRank(oldValue, "SHIELD") and Schema:IsCombineRank(value, "SHIELD")) then
 		character:JoinClass(CLASS_MPS)
 	end
-
-	if (!Schema:IsCombineRank(oldValue, "GHOST") and Schema:IsCombineRank(value, "GHOST")) then
-		character:SetModel("models/eliteghostcp.mdl")
-	end
-	]]
 end
 
 FACTION_MPF = FACTION.index
