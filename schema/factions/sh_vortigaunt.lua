b5ff63232d59be876cda275621d00e5812862208
xTheLifex <nicholastoya@yahoo.com.br>
2022-10-12 00:30:04 -0300

    Vortigaunts

---
+++ schema/factions/sh_vortigaunt.lua
@@ -0,0 +1,21 @@
+FACTION.name = "Vortigaunt"
+FACTION.description = "The Vortigaunts are a sapient alien species found throughout the Half-Life series."
+FACTION.color = Color(61, 155, 61)
+FACTION.isDefault = true
+FACTION.models = {
+	"models/vortigaunt.mdl"
+}
+
+function FACTION:OnCharacterCreated(client, character)
+	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
+	local inventory = character:GetInventory()
+
+	character:SetData("cid", id)
+
+	inventory:Add("cid", 1, {
+		name = character:GetName(),
+		id = id
+	})
+end
+
+FACTION_VORT = FACTION.index
