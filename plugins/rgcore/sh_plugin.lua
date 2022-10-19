6c8e18b120b75515c01e579ef8edd7eefc83e371
xTheLifex <nicholastoya@yahoo.com.br>
2022-10-19 17:12:39 -0300

    Play sound

--- plugins/rgcore/sh_plugin.lua
+++ plugins/rgcore/sh_plugin.lua
@@ -38,8 +38,8 @@ ix.util.Include("sub/sh_commands.lua")
 ix.util.Include("sub/sh_voicecommands.lua")
 ix.util.Include("sub/sh_skins.lua")
 ix.util.Include("sub/sh_relationships.lua")
+ix.util.Include("sub/sh_voices.lua")
 ix.util.Include("sub/clothing/sh_clothing.lua")
-
 
 ix.option.Add("drawCrosshair", ix.type.bool, true, {
    category = "appearance"
@@ -49,6 +49,38 @@ ix.lang.AddTable("english", {
    optDrawCrosshair = "Draw crosshair"
 })
 
+
+do
+	local COMMAND = {}
+	COMMAND.arguments = ix.type.text
+
+	function COMMAND:OnRun(client, path)
+		if (client:IsAdmin() or client:IsSuperAdmin()) then
+         net.Start("ulib_sound")
+         net.WriteString(Sound(path) or "")
+         net.Broadcast()
+      else
+         client:Notify("You must be an admin to do this!")
+      end
+	end
+
+	ix.command.Add("PlaySound", COMMAND)
+end
+
+do
+	local COMMAND = {}
+	COMMAND.arguments = ix.type.text
+
+	function COMMAND:OnRun(client, path)
+		if (client:IsAdmin() or client:IsSuperAdmin()) then
+         client:EmitSound(path)
+      else
+         client:Notify("You must be an admin to do this!")
+      end
+	end
+
+	ix.command.Add("PlayLocalSound", COMMAND)
+end
 
 do
 	local COMMAND = {}
@@ -80,6 +112,9 @@ if (SERVER) then
 
    function PLUGIN:PlayerMessageSend(speaker, chatType, text, anonymous, receivers, rawText)
       --self:DoSpeakingGestures(speaker, chatType, text, anonymous, receivers, rawText)
+      if (chatType == "ic") then
+         ix.log.Add(speaker, "chat", "RG-Core", text)
+      end
    end
 end
 
@@ -87,5 +122,43 @@ if (CLIENT) then
    function PLUGIN:ShouldDrawCrosshair(client, weapon)
       local shouldDraw = ix.option.Get("drawCrosshair", true)
       if (!shouldDraw) then return false end
+   end
+
+   local red = {
+      ["pistol"] = true,
+      ["smg1"] = true,
+      ["ar2"] = true,
+      ["rpg"] = true
+   }
+   local yellow = {
+
+   }
+   local green = {
+
+   }
+
+   local c_red = Color(255, 0, 0)
+   local c_yellow = Color(185,150,54)
+   local c_green = Color(26,126,26)
+
+
+
+   function PLUGIN:PopulateItemTooltip(tooltip, item)
+      if (LocalPlayer():IsCombine()) then
+         if (red[item.id] or yellow[item.id] or green[item.id]) then
+            local row = tooltip:AddRow("ContrabandAlert")
+            local color = c_green
+
+            if (red[item.id]) then color = c_red end
+            if (yellow[item.id]) then color = c_yellow end
+            if (green[item.id]) then color = c_green end
+
+            row:SetBackgroundColor(color)
+            row:SetText("This item is considered contraband by the Universal Union")
+            row:SetFont("BudgetLabel")
+            row:SetExpensiveShadow(0.5)
+            row:SizeToContents()
+         end
+      end
    end
 end
